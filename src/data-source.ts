5dd5da871b531b7084b4d188c7381b95595483a5
Lucas <lucas-leite-menezes@hotmail.com>
2023-03-29 14:12:30 -0300

    fixing data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,26 +1,50 @@
-import { DataSource, DataSourceOptions } from "typeorm";
-import path from "path";
-import "dotenv/config";
 import "reflect-metadata";
+import "dotenv/config";
+import path from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
+import { Client } from "./entities/client.entity";
+import { Contact } from "./entities/contact.entity";
 
-export const setDataSourceConfig = new DataSource(
-  process.env.NODE_ENV === "test"
-    ? {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"],
-      }
-    : {
-        type: "postgres",
-        host: process.env.PGHOST,
-        port: 5432,
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
-        logging: true,
-        synchronize: false,
-        entities: [path.join(__dirname, "./entities/**.{js,ts}")],
-        migrations: [path.join(__dirname, "./migrations/**.{js,ts}")],
-      }
-);
+const setDataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
+  const migrationsPath: string = path.join(
+    __dirname,
+    "./migrations/**.{js,ts}"
+  );
+
+  const nodeEnv = process.env.NODE_ENV;
+
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities: [Client, Contact],
+      migrations: [migrationsPath],
+    };
+  }
+
+  if (nodeEnv === "test") {
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: [Client, Contact],
+    };
+  }
+
+  return {
+    type: "postgres",
+    host: process.env.PGHOST,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: 5432,
+    database: process.env.DATABASE,
+    synchronize: false,
+    logging: true,
+    entities: [Client, Contact],
+    migrations: [migrationsPath],
+  };
+};
+
+const dataSourceConfig = setDataSourceConfig();
+export default new DataSource(dataSourceConfig);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-import { setDataSourceConfig } from "./data-source";
+import setDataSourceConfig from "./data-source";
 
 (async () => {
   await setDataSourceConfig.initialize().catch((err) => {
