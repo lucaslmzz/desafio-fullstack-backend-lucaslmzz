fcf39cf4af7541d2ef2d039b6d6b2ed18d8484a7
Lucas <lucas-leite-menezes@hotmail.com>
2023-03-31 14:34:07 -0300

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import { handleError } from "./errors/handleError";
 import { clientRouter } from "./routes/client.routes";
 import { contactRouter } from "./routes/contact.routes";
 import loginRouter from "./routes/login.routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/clients", clientRouter);
 app.use("/login", loginRouter);
 app.use("/contacts", contactRouter);
